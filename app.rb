38b91dc4a6bb2ed70a5c65069db365d3a3799fe9
Ariel <arielord@buffalo.edu>
2019-03-28 02:42:00 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -7,7 +7,7 @@ class App < Sinatra::Base
 
   post '/reverse' do
     @reversed_string = params[:string]
-    @reversed_string = @string.reverse
+    @reversed_string.reverse
     erb :reversed
   end
 
